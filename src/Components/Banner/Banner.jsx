f6c1355d9576f4f3fd34a2e1c4324b3c778a76b7
Namul <namul@Namuls-MacBook-Air.local>
2026-04-07 16:04:23 +0600

    banner section done

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { VscDebugStart } from 'react-icons/vsc';
+import bannerImg from '../../assets/banner.png';
+import badgeImg from '../../assets/Group 5.png';
+
+const Banner = () => {
+    return (
+        <div className='container mx-auto py-20'>
+            <div className='lg:flex justify-between items-center gap-14'>
+
+                <div className='space-y-4'>
+                    <div className="badge badge-soft badge-primary">
+                        <img src={badgeImg} alt="badge logo" />
+                        New: AI-Powered Tools Available
+                    </div>
+
+                    <h1 className='text-7xl font-extrabold text-[#101727]'>
+                        Supercharge Your <br /> Digital Workflow
+                    </h1>
+
+                    <div className='flex gap-4'>
+                        <button className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full border border-[#4F39F6] 
+                         hover:bg-linear-to-r hover:from-white hover:to-white hover:text-[#4F39F6]">
+                            Explore Products
+                        </button>
+
+                        <button className="btn bg-transparent border border-[#4F39F6] text-[#4F39F6] rounded-full hover:bg-linear-to-r hover:from-[#4F39F6] hover:to-[#9514FA] hover:text-white">
+                            <VscDebugStart /> Watch Demo
+                        </button>
+                    </div>
+                </div>
+
+                <div>
+                    <img className='w-full' src={bannerImg} alt="" />
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
